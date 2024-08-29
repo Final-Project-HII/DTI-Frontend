a586b3b15f69ee0622d4da6a149181cbc0a3ed53
Hendry Tjahaja <hendrysurijanto123@gmail.com>
2024-08-30 00:33:20 +0700

    Feat(Landing Page & Footer):Feature for landing page and footer is done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
+
+import Footer from "@/components/Footer";
 import Image from "next/image";
+import Carousel from "./_components/Carousel";
+import CategoryList from "./_components/CategoryList";
+import ReasonToShop from "./_components/ReasonToShop";
+import DownloadApp from "./_components/DownloadApp";
+import ProductList from "./_components/ProductList";
 
 
 export default function Home() {
   return (
     <>
-      <h1>ahahahha</h1>
+      <Carousel />
+      <CategoryList />
+      <ProductList />
+      <ReasonToShop />
+      <DownloadApp />
+      <Footer />
     </>
   );
 }
