eab5e29423cdfc61a8fafb36021044c13e3dce41
001indah <indahbudiati001@gmail.com>
2024-08-30 15:10:59 +0700

    feat(admin): add product and category management features
    Implement CRUD operations for products and categories in the admin panel to allow better control over inventory. This includes the ability to add, edit, delete, and view products and categories.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Providers from "./providers";
 
-const inter = Inter({ subsets: ["latin"] });
+
+const JakartaSans = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +18,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={JakartaSans.className}>
+        <Providers>
+
+          {children}
+        </Providers>
+      </body>
     </html>
+
   );
 }
