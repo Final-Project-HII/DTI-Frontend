3b7559444be9415f89d5b77726e9d67abc3a4fa0
Hendry Tjahaja <hendrysurijanto123@gmail.com>
2024-09-01 17:05:25 +0700

    Feat(NavBar):Navbar is done

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,49 +1,61 @@
 'use client'
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { LayoutGrid, Search, ShoppingCart, Menu, ChevronDown, ArrowLeft } from 'lucide-react';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../ui/dropdown-menu';
-import { Button } from '../ui/button';
-import { Input } from '../ui/input';
-import { Sheet, SheetClose, SheetContent, SheetTrigger } from '../ui/sheet';
-import { Badge } from '../ui/badge';
-import { IoIosListBox } from 'react-icons/io';
-import { FaSearch, FaTimes } from 'react-icons/fa';
+import avatar from '@/public/promo1.jpg';
 import { useMediaQuery } from "@uidotdev/usehooks";
-import SearchSheet from '../SearchSheet';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { ChevronDown, LogOutIcon, ShoppingCart } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { FaEdit } from 'react-icons/fa';
+import { IoIosListBox } from 'react-icons/io';
 import 'swiper/css';
+import { Badge } from '../ui/badge';
+import { Button } from '../ui/button';
+import CategoryDropdown from './_components/CategoryDropdown';
+import CategorySwiper from './_components/CategorySwiper';
+import SearchInput from './_components/SearchInput';
+import SearchSheet from './_components/SearchSheet';
+import { useSession } from 'next-auth/react';
 
 const NavBar = () => {
   const itemCount = 5;
-  const categories = ['Electronics', 'Clothing', 'Books', 'Home & Garden', 'Electronics', 'Clothing', 'Books', 'Home & Garden'];
-  const [open, setOpen] = useState(false);
-  const isDesktop = useMediaQuery('(min-width: 1024px)');
-  const [isFocused, setIsFocused] = useState(false);
+  const [openHamburgerMenu, setOpenHamburgerMenu] = useState(false)
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [openDropdownMenu, setOpenDropdownMenu] = useState(false);
+  const { data } = useSession();
 
-  const handleFocus = () => {
-    setIsFocused(true);
-  };
-
-  const handleBlur = () => {
-    setIsFocused(false);
-  };
 
   const toggleMenu = () => {
-    setOpen(!open);
+    setOpenHamburgerMenu(prev => !prev)
+  };
+
+  const handleToggleDropdown = () => {
+    setOpenDropdownMenu(prev => !prev);
   };
 
   useEffect(() => {
-    if (isDesktop && open) {
-      setOpen(false);
+    const checkIsDesktop = () => {
+      setIsDesktop(window.innerWidth >= 1024);
+    };
+
+    checkIsDesktop();
+    window.addEventListener('resize', checkIsDesktop);
+
+    return () => window.removeEventListener('resize', checkIsDesktop);
+  }, []);
+
+  useEffect(() => {
+    if (isDesktop && openHamburgerMenu) {
+      setOpenHamburgerMenu(false);
     }
-  }, [isDesktop, open]);
+    if (!isDesktop && openDropdownMenu) {
+      setOpenDropdownMenu(false);
+    }
+  }, [isDesktop, openHamburgerMenu]);
 
   return (
     <header className="fixed top-0 w-full z-50 text-white bg-no-repeat bg-cover">
       <div className="relative">
-        {/* Main navbar content */}
-        <div className="relative z-20 text-white bg-no-repeat bg-cover py-4" style={{ backgroundImage: 'url("/header.svg")' }}>
+        <div className="relative z-40 text-white bg-no-repeat bg-cover py-4" style={{ backgroundImage: 'url("/header.svg")' }}>
           <div className="flex px-4 items-center justify-between lg:px-10">
             <div className="flex items-center space-x-4">
               <button
@@ -51,13 +63,13 @@ const NavBar = () => {
                 onClick={toggleMenu}
               >
                 <span
-                  className={`block w-6 h-1 bg-blue-500 rounded-sm transform transition-transform duration-300 ease-in-out ${open ? "rotate-45 translate-y-1" : ""}`}
+                  className={`block w-6 h-1 bg-blue-500 rounded-sm transform transition-transform duration-300 ease-in-out ${openHamburgerMenu ? "rotate-45 translate-y-1" : ""}`}
                 ></span>
                 <span
-                  className={`block w-6 h-1 bg-blue-500 rounded-sm transform transition-transform duration-300 ease-in-out ${open ? "opacity-0" : "my-1"}`}
+                  className={`block w-6 h-1 bg-blue-500 rounded-sm transform transition-transform duration-300 ease-in-out ${openHamburgerMenu ? "opacity-0" : "my-1"}`}
                 ></span>
                 <span
-                  className={`block w-6 h-1 bg-blue-500 rounded-sm transform transition-transform duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-1" : ""}`}
+                  className={`block w-6 h-1 bg-blue-500 rounded-sm transform transition-transform duration-300 ease-in-out ${openHamburgerMenu ? "-rotate-45 -translate-y-1" : ""}`}
                 ></span>
               </button>
               <div className="hidden lg:block lg:text-2xl font-bold text-blue-600 italic">
@@ -65,41 +77,11 @@ const NavBar = () => {
               </div>
               <img src="/hiimart v6.png" alt="HiiMart Logo" className="w-24 h-auto hidden lg:block" />
               <div className="hidden lg:block">
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" className="text-blue-600">
-                      <LayoutGrid className="mr-2 h-5 w-5" />
-                      Category
-                      <ChevronDown className="ml-2 h-4 w-4" />
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent className="bg-white">
-                    {categories.map((category, index) => (
-                      <DropdownMenuItem key={index}>
-                        <Link href={`/category/${category.toLowerCase()}`}>{category}</Link>
-                      </DropdownMenuItem>
-                    ))}
-                  </DropdownMenuContent>
-                </DropdownMenu>
+                <CategoryDropdown />
               </div>
             </div>
-
             <div className="flex-grow mx-1 lg:mx-4 max-w-xl hidden lg:block">
-              <div className="relative">
-                <Input
-                  type="text"
-                  placeholder="Search products..."
-                  className="w-full pl-3 pr-16 py-2 rounded-md text-black"
-                  onFocus={handleFocus}
-                  onBlur={handleBlur}
-                />
-                <Button
-                  size="sm"
-                  className="absolute right-1 top-1/2 transform -translate-y-1/2 bg-yellow-300 hover:bg-yellow-400 rounded-lg p-1 px-3 mx-2"
-                >
-                  {isFocused ? <FaTimes className="h-5 w-5 text-blue-600" /> : <Search className="h-5 w-5 text-blue-600" />}
-                </Button>
-              </div>
+              <SearchInput />
             </div>
 
             <div className="flex items-center lg:space-x-4 space-x-1">
@@ -111,10 +93,33 @@ const NavBar = () => {
                 <ShoppingCart className="h-6 w-6 text-blue-600" />
                 <Badge className="absolute -top-2 -right-2 bg-red-500">{itemCount}</Badge>
               </Button>
-              <div className="hidden lg:flex space-x-2">
-                <Link href='/register'> <Button variant="outline" className="bg-white text-blue-600 hover:bg-blue-50">Sign Up</Button></Link>
-                <Link href='/login'><Button className="bg-blue-600 text-white hover:bg-blue-700">Login</Button></Link>
-              </div>
+              {data?.user.role == 'USER' ? (
+                <div className=" items-center w-32 relative hidden lg:flex">
+                  <div className="flex items-center hover:bg-gray-50 hover:bg-opacity-40 gap-2 px-1 py-[2px]" onClick={handleToggleDropdown}>
+                    <Image src={avatar} width={25} height={25} className='rounded-full object-fit h-7 w-7' alt='' />
+                    <h3 className='font-semibold text-sm text-black line-clamp-1'>Hendry Tjahaja Surijanto Putra</h3>
+                    <ChevronDown height={40} width={40} className='text-black' />
+                  </div>
+                  {openDropdownMenu && <div className="absolute bottom-0 w-60  translate-y-full -left-full bg-white rounded-xl  ">
+                    <div className="flex justify-between items-center px-5 py-2 border-b-2 border-gray-100 group hover:bg-blue-600">
+                      <div className="flex gap-4 items-center">
+                        <h3 className='font-semibold text-xs text-black line-clamp-1 group-hover:text-white'>Hai, Hendry Tjahaja Surijanto Putra</h3>
+                      </div>
+                      <FaEdit width={20} height={20} className='text-blue-600 group-hover:text-white' />
+                    </div>
+                    <div className="flex items-center px-5 py-2 gap-4 border-b-2 border-gray-100 hover:bg-blue-600 group">
+                      <LogOutIcon width={16} height={16} className='text-red-600' />
+                      <h3 className='text-xs font-semibold text-black group-hover:text-white'>Logout</h3>
+                    </div>
+                  </div>}
+                </div>
+              ) : (
+                <div className="hidden lg:flex space-x-2">
+                  <Link href='/register'> <Button variant="outline" className="bg-white text-blue-600 hover:bg-blue-50">Sign Up</Button></Link>
+                  <Link href='/login'><Button className="bg-blue-600 text-white hover:bg-blue-700">Login</Button></Link>
+                </div>
+              )}
+
             </div>
           </div>
         </div>
@@ -124,27 +129,33 @@ const NavBar = () => {
             ? '-translate-y-full'
             : '-translate-y-0'}
                          ${isDesktop ? 'z-10' : 'z-30'}`}>
-          <Swiper
-            spaceBetween={10}
-            slidesPerView={6}
-            freeMode={true}
-            grabCursor={true}
-            breakpoints={{
-              320: {
-                slidesPerView: 4,
-              },
+          <CategorySwiper />
 
-              768: {
-                slidesPerView: 6,
-              },
-            }}
-          >
-            {categories.map((category, index) => (
-              <SwiperSlide key={index} className="w-auto">
-                <h2 className="text-black">{category}</h2>
-              </SwiperSlide>
-            ))}
-          </Swiper>
+        </div>
+        <div className={`absolute z-30  text-xl text-blue-600 font-bold w-screen bottom-0 bg-white  transition-transform duration-500 ease-in-out ${openHamburgerMenu ? "translate-y-full h-screen" : "translate-y-0 overflow-hidden"}`}>
+          {data?.user.role == 'USER' ? (
+            <>
+              <div className="flex justify-between items-center px-5 py-2 border-b-2 border-gray-100">
+                <div className="flex gap-4 items-center">
+                  <Image src={avatar} width={25} height={25} className='rounded-full object-fit h-7 w-7' alt='' />
+                  <h3 className='font-semibold text-sm text-black'>Hai, Hendry Tjahaja Surijanto Putra</h3>
+                </div>
+                <FaEdit width={20} height={20} className='text-black' />
+              </div>
+              <div className="flex items-center px-5 py-2 gap-4 border-b-2 border-gray-100">
+                <LogOutIcon width={25} height={25} className='text-red-600' />
+                <h3 className='text-sm text-black'>Logout</h3>
+              </div>
+            </>
+          ) : (
+            <>
+              <Link href="/login" onClick={() => setOpenHamburgerMenu(false)}>
+                <h1 className='text-blue-600 border-b-2 border-gray-100 px-5 py-4 hover:text-white hover:bg-blue-600'>Login</h1>
+              </Link>
+              <Link href="/register" onClick={() => setOpenHamburgerMenu(false)}>
+                <h1 className='text-blue-600 border-b-2 border-gray-100 px-5 py-4  hover:text-white hover:bg-blue-600'>Register</h1>
+              </Link>
+            </>)}
         </div>
       </div>
     </header >
