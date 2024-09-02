ce8266c08c4494f9c3f26534e62e9aa37e391897
Hendry Tjahaja <80196576+akaslayer@users.noreply.github.com>
2024-09-02 08:55:14 +0700

    Feat(Navbar):Feature for navbar is done (#10)
    * Fix(Auth):Fix the register with google
    
    * Feat(Landing Page & Footer):Feature for landing page and footer is done
    
    * Fix(Footer):Fix the footer layout
    
    * fix(Development):Fix the conflict developent branch
    
    * Feat(Landing Page):Add some skeleton
    
    * Fix(Auth):Fix jwt token session in next-auth
    
    * merge
    
    * Feat(NavBar):Navbar is done
    
    * Feat(NavBar):Feature for navbar is done

--- app/(main)/product/_components/ProductCard.tsx
+++ app/(main)/product/_components/ProductCard.tsx
@@ -41,23 +41,19 @@ export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
         <Card className="flex flex-col h-full bg-white hover:scale-105 ease-in-out duration-300">
             <Link href={`/product/${product.id}_${product.name.replace(/\s+/g, '-').toLowerCase()}`} passHref>
                 <CardHeader className="relative p-0 mb-4">
-<<<<<<< HEAD
                     <Image
                         width={200}
                         height={200}
                         src={product.productImages[0]?.imageUrl || '/placeholder.jpg'}
-=======
-                    <img
-                        src={product.productImages[0]?.imageUrl || '/header.svg'}
->>>>>>> 74689e7eed19bd30dcb10a5a1a6c46446c53c966
+
                         alt={product.name}
                         className="w-full h-[150px] lg:h-[200px] object-fit rounded-md"
                     />
                     <Badge className="absolute top-3 right-3 bg-white text-blue-600">
                         {product.categoryName}
                     </Badge>
-                </CardHeader>
-            </Link>
+                </CardHeader >
+            </Link >
             <CardContent className="px-4 pb-4 flex-grow mb-0">
                 <CardTitle className="mb-2 text-md">{product.name}</CardTitle>
                 <p className="font-bold text-orange-500 my-2 text-base">Rp {product.price.toLocaleString()}</p>
@@ -65,12 +61,8 @@ export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
                 <p className="text-xs">Category: {product.categoryName}</p>
             </CardContent>
             <CardFooter className="px-4 pb-4 mt-auto">
-<<<<<<< HEAD
                 <Button className="w-full border-2 border-blue-600 text-blue-600 bg-white hover:bg-blue-600 hover:text-white"> + Add to Cart</Button>
-=======
-                <Button className="w-full border border-blue-600 text-blue-600 bg-transparent hover:bg-transparent">+ Add to Cart</Button>
->>>>>>> 74689e7eed19bd30dcb10a5a1a6c46446c53c966
-            </CardFooter>
-        </Card>
+            </CardFooter >
+        </Card >
     );
 };
