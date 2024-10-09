6a68de758bb2823fcf4e62898a96df474f95fe4d
iqballl <155224155+iqbalya25@users.noreply.github.com>
2024-10-09 20:01:41 +0700

    Feature/admin ordermng 5 (#54)
    * filter warehouse in admin order
    
    * fixing useadminorder endpoint
    
    * fixing payment method to order admin management
    
    * fixing order page
    
    * styling order admin management
    
    * fixing filter for user order
    
    * fixing navbar count item
    
    * fixing cart detail page
    
    * fixing admin order SUPER and ADMIN
    
    * pull from Development
    
    * adding sequence order status change
    
    * fixing sequence order status change
    
    * fixing sequence order status change
    
    * fixing from pull Development 00:46
    
    * fixing from pull Development 00:49
    
    * fixing from pull Development 00:53
    
    * fixing env for product and categories
    
    * fixing env for product and categories
    
    * fixing Admin Order Table
    
    * fixing page [delete layout.tsx]
    
    * fixing navbar, cart detail page and order status modal
    
    * fixing product card

--- app/(user)/(main)/product/_components/ProductCard.tsx
+++ app/(user)/(main)/product/_components/ProductCard.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Badge } from '@/components/ui/badge';
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
 import { Plus, ShoppingBag } from "lucide-react";
 import { useCart } from "@/hooks/useCart";
-
+import Swal from "sweetalert2";
 
 interface ProductCardProps {
   product: Product;
@@ -35,18 +41,28 @@ interface ProductImage {
 }
 
 export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
-  // const { addToCart } = useCart();
+  const { addToCart } = useCart();
 
-  // const handleAddToCart = async () => {
-  //   try {
-  //     await addToCart(product.id, 1);
-  //     alert("Product added to cart!");
-  //   } catch (error) {
-  //     console.error("Failed to add product to cart:", error);
-  //     alert("Failed to add product to cart.");
-  //   }
-  // };
-
+  const handleAddToCart = async () => {
+    try {
+      await addToCart(product.id, 1);
+      Swal.fire({
+        title: "Product added to cart!",
+        icon: "success",
+        timer: 2000,
+        showConfirmButton: false,
+        timerProgressBar: true,
+      });
+      console.log("After Swal.fire");
+    } catch (error) {
+      console.error("Failed to add product to cart:", error);
+      Swal.fire({
+        title: "Error",
+        text: "Failed to add product to cart.",
+        icon: "error",
+      });
+    }
+  };
   const truncateDescription = (description: string, maxLength: number) => {
     if (description.length > maxLength) {
       return description.slice(0, maxLength) + "...";
@@ -56,15 +72,22 @@ export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
 
   return (
     <Card className="flex flex-col h-full bg-white hover:shadow-lg transition-all transform hover:scale-105 duration-200 ease-in-out border-none">
-      <Link href={`/product/${product.id}_${product.name.replace(/\s+/g, '-').toLowerCase()}`} passHref>
+      <Link
+        href={`/product/${product.id}_${product.name
+          .replace(/\s+/g, "-")
+          .toLowerCase()}`}
+        passHref
+      >
         <CardHeader className="relative p-0 h-48">
           <Image
-            src={product.productImages[0]?.imageUrl || '/placeholder.jpg'}
+            src={product.productImages[0]?.imageUrl || "/placeholder.jpg"}
             alt={product.name}
             layout="fill"
             objectFit="cover"
-            className={`rounded-t-lg ${product.totalStock === 0 ? 'grayscale' : ''}`}
-          // style={{ filter: product.totalStock === 0 ? 'grayscale(100%)' : 'none' }}
+            className={`rounded-t-lg ${
+              product.totalStock === 0 ? "grayscale" : ""
+            }`}
+            // style={{ filter: product.totalStock === 0 ? 'grayscale(100%)' : 'none' }}
           />
           <Badge className="absolute top-3 right-3 bg-gray-100 text-blue-600 hover:bg-transparent-none">
             {product.categoryName}
@@ -72,22 +95,28 @@ export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
         </CardHeader>
       </Link>
       <CardContent className="flex-grow flex flex-col p-4">
-        <CardTitle className="text-md mb-2 line-clamp-2 h-12">{product.name}</CardTitle>
+        <CardTitle className="text-md mb-2 line-clamp-2 h-12">
+          {product.name}
+        </CardTitle>
         <div className="flex items-center text-xs text-blue-600 mb-2">
           <ShoppingBag className="w-3 h-3 mr-1" />
           <span>Hiimart Store</span>
         </div>
-        <p className="font-bold text-orange-500 text-lg mt-auto">Rp {product.price.toLocaleString()}</p>
+        <p className="font-bold text-orange-500 text-lg mt-auto">
+          Rp {product.price.toLocaleString()}
+        </p>
         <p className="text-xs text-gray-500">Weight: {product.weight}g</p>
       </CardContent>
       <CardFooter className="p-4 pt-0">
         <Button
           className="w-full border border-blue-600 text-blue-600 bg-transparent hover:bg-blue-50"
           disabled={product.totalStock === 0}
-        // onClick={handleAddToCart}
+          onClick={handleAddToCart}
         >
-          {product.totalStock > 0 && <Plus className='w-4 h-4 mr-2' strokeWidth={3} />}
-          {product.totalStock === 0 ? 'Out of Stock' : 'Add to Cart'}
+          {product.totalStock > 0 && (
+            <Plus className="w-4 h-4 mr-2" strokeWidth={3} />
+          )}
+          {product.totalStock === 0 ? "Out of Stock" : "Add to Cart"}
         </Button>
       </CardFooter>
     </Card>
