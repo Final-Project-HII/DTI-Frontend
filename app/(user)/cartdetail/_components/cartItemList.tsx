c611105cb5c9e7dbc3e3080962cf28e3fb55816c
iqballl <155224155+iqbalya25@users.noreply.github.com>
2024-10-21 22:55:02 +0700

    Feature/admin ordermng 8 (#85)
    * adding from development improvement (filter date user order, pagination admin order, status shipped, search bar user order
    
    * fixing adding product to cart (limiting as stock)

--- app/(user)/cartdetail/_components/cartItemList.tsx
+++ app/(user)/cartdetail/_components/cartItemList.tsx
@@ -3,6 +3,8 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useCart } from "@/hooks/useCart";
 import debounce from "lodash/debounce";
+import { ProductDataResponse } from "@/types/product";
+import { useProductDetails } from "@/hooks/useProduct";
 
 interface CartItem {
   id: number;
@@ -23,6 +25,9 @@ const CartItemList: React.FC<CartItemListProps> = ({ initialCartItems }) => {
   const [cartItems, setCartItems] = useState<CartItem[]>(initialCartItems);
   const { updateQuantity, removeItem } = useCart();
 
+  const productIds = cartItems.map((item) => item.productId);
+  const productDetails = useProductDetails(productIds);
+
   const debouncedUpdateQuantity = useCallback(
     debounce(async (productId: number, newQuantity: number) => {
       await updateQuantity(productId, newQuantity);
@@ -30,10 +35,14 @@ const CartItemList: React.FC<CartItemListProps> = ({ initialCartItems }) => {
     [updateQuantity]
   );
 
-  const handleUpdateQuantity = (productId: number, newQuantity: number) => {
+  const handleUpdateQuantity = (
+    productId: number,
+    newQuantity: number,
+    totalStock: number
+  ) => {
     if (newQuantity === 0) {
       handleRemoveItem(productId);
-    } else {
+    } else if (newQuantity <= totalStock) {
       setCartItems((prevItems) =>
         prevItems.map((item) =>
           item.productId === productId
@@ -68,57 +77,76 @@ const CartItemList: React.FC<CartItemListProps> = ({ initialCartItems }) => {
       <h2 className="text-xl font-bold mb-4">
         Hii Mart ({cartItems.length} product{cartItems.length !== 1 ? "s" : ""})
       </h2>
-      {cartItems.map((item) => (
-        <div
-          key={item.id}
-          className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-4 border-b pb-4"
-        >
-          <div className="flex items-center mb-2 sm:mb-0">
-            <Image
-              src={
-                item.productDetails.productImages[0]?.imageUrl ||
-                "/placeholder-image.jpg"
-              }
-              alt={item.productName}
-              width={64}
-              height={64}
-              className="object-cover rounded-md"
-            />
-            <div className="ml-4">
-              <h3 className="font-bold">{item.productName}</h3>
-              <p className="text-blue-600">Rp {item.price.toLocaleString()}</p>
+      {cartItems.map((item, index) => {
+        const productDetail = productDetails[index].data as
+          | ProductDataResponse
+          | undefined;
+        const totalStock = productDetail?.totalStock || 0;
+
+        return (
+          <div
+            key={item.id}
+            className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-4 border-b pb-4"
+          >
+            <div className="flex items-center mb-2 sm:mb-0">
+              <Image
+                src={
+                  item.productDetails.productImages[0]?.imageUrl ||
+                  "/placeholder-image.jpg"
+                }
+                alt={item.productName}
+                width={64}
+                height={64}
+                className="object-cover rounded-md"
+              />
+              <div className="ml-4 flex-grow">
+                <h3 className="font-bold">{item.productName}</h3>
+                <p className="text-blue-600">
+                  Rp {item.price.toLocaleString()}
+                </p>
+                <p className="text-sm text-gray-500">Stock: {totalStock}</p>
+              </div>
+            </div>
+            <div className="flex items-center justify-between w-full sm:w-auto mt-2 sm:mt-0">
+              <div className="flex items-center mr-4">
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="w-8 h-8 rounded-md bg-blue-500 text-white hover:bg-blue-600"
+                  onClick={() =>
+                    handleUpdateQuantity(
+                      item.productId,
+                      item.quantity - 1,
+                      totalStock
+                    )
+                  }
+                >
+                  -
+                </Button>
+                <span className="mx-2 w-8 text-center">{item.quantity}</span>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="w-8 h-8 rounded-md bg-blue-500 text-white hover:bg-blue-600"
+                  onClick={() =>
+                    handleUpdateQuantity(
+                      item.productId,
+                      item.quantity + 1,
+                      totalStock
+                    )
+                  }
+                  disabled={item.quantity >= totalStock}
+                >
+                  +
+                </Button>
+              </div>
+              <p className="w-28 sm:w-32 text-right font-bold whitespace-nowrap">
+                Rp {(item.price * item.quantity).toLocaleString()}
+              </p>
             </div>
           </div>
-          <div className="flex items-center justify-between w-full sm:w-auto mt-2 sm:mt-0">
-            <div className="flex items-center mr-4">
-              <Button
-                variant="outline"
-                size="sm"
-                className="w-8 h-8 rounded-md bg-blue-500 text-white hover:bg-blue-600"
-                onClick={() =>
-                  handleUpdateQuantity(item.productId, item.quantity - 1)
-                }
-              >
-                -
-              </Button>
-              <span className="mx-2 w-8 text-center">{item.quantity}</span>
-              <Button
-                variant="outline"
-                size="sm"
-                className="w-8 h-8 rounded-md bg-blue-500 text-white hover:bg-blue-600"
-                onClick={() =>
-                  handleUpdateQuantity(item.productId, item.quantity + 1)
-                }
-              >
-                +
-              </Button>
-            </div>
-            <p className="w-24 text-right font-bold">
-              Rp {(item.price * item.quantity).toLocaleString()}
-            </p>
-          </div>
-        </div>
-      ))}
+        );
+      })}
       <Button
         variant="outline"
         className="text-blue-600 border-blue-600 hover:bg-blue-50 mt-4"
